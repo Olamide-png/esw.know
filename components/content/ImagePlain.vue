aeef6d9105317ee5538210939efb3522763b2ee2
Olamide-png <otoyinbo@eshopworld.com>
2025-06-04 08:58:42 +0100

    Implement code changes to enhance functionality and improve performance

---
+++ components/content/ImagePlain.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-5">
+    <NuxtLink :to="src" :target="target">
+      <div class="p-4">
+        <NuxtImg :src="src" :alt="alt" class="rounded-lg mx-auto" />
+      </div>
+    </NuxtLink>
+    <p class="text-center my-4 text-sm text-muted-foreground">
+      {{ alt }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  src: string;
+  target?: string;
+  alt?: string;
+}
+withDefaults(defineProps<Props>(), {
+  target: "_blank",
+});
+</script>
