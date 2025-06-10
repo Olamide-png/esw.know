ab38bfd4419cf542395c1217fc83e08f9ddd9926
Olamide-png <otoyinbo@eshopworld.com>
2025-06-10 17:53:44 +0100

    fix: update default highlight theme to 'quietlight'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,7 +48,7 @@ export default defineNuxtConfig({
     documentDriven: true,
     highlight: {
       theme: {
-        default: 'github-light',
+        default: 'quietlight',
         dark: 'dracula',
       },
       preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'diff', 'shell', 'markdown', 'mdc', 'yaml', 'bash', 'ini', 'dotenv'],
