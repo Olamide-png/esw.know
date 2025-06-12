f77bca8a7e4fb13cda6527535aacff91b57c4d38
Olamide-png <otoyinbo@eshopworld.com>
2025-06-12 14:28:46 +0100

    apis link

--- www/app.config.ts
+++ www/app.config.ts
@@ -65,7 +65,7 @@ export default defineAppConfig({
         icon: 'lucide:book-open',
       }, {
         title: 'APIs',
-        to: '/apis/index.md',
+        to: '/apis/index',
         target: '_blank',
         showLinkIcon: true,
         description: 'API documentation for ESW services',
