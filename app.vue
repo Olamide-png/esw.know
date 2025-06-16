cabec1f043389226fa278697ca884040e7859abd
Olamide-png <otoyinbo@eshopworld.com>
2025-06-16 10:22:12 +0100

    screen

--- app.vue
+++ app.vue
@@ -7,10 +7,10 @@
       class="flex-1 items-start px-4 md:grid md:gap-6 md:px-8 lg:gap-10"
       :class="[
         config.main.padded && 'container',
-        (page.aside ?? true) && 'md:grid-cols-[220px_minmax(0,1fr)] lg:grid-cols-[240px_minmax(0,1fr)]',
+        (page.aside ?? true) && 'md:grid-cols-[280px_minmax(0,1fr)] lg:grid-cols-[320px_minmax(0,1fr)]',
       ]"
     >
-      <aside v-if="page.aside ?? true" class="fixed top-[102px] z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto md:sticky md:top-[60px] md:block">
+      <aside v-if="page.aside ?? true" class="fixed top-[102px] z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] md:w-[280px] lg:w-[320px] shrink-0 overflow-y-auto md:sticky md:top-[60px] md:block">
         <LayoutAside :is-mobile="false" />
       </aside>
       <NuxtPage />
