97dcc912f66b6e12f02f582cf91cb8b135c698c5
Olamide-png <otoyinbo@eshopworld.com>
2025-06-30 04:26:56 +0100

    kbd

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -63,7 +63,7 @@ export default {
         sm: 'calc(var(--radius) - 4px)',
       },
       boxShadow: {
-        outer: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        outer: '0 2px 10px hsl(var(--shadow))',
         inner: 'inset 0 2px 10px hsl(var(--shadow))',
         custom: '0 1px 3px hsl(var(--shadow))',
       },
