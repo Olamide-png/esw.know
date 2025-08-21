9bd1ae4f4faf7c477f81e71f7e28e4c4a3eebfd9
Olamide-png <otoyinbo@eshopworld.com>
2025-08-21 16:56:43 +0100

    logo size

--- components/layout/Header/Logo.vue
+++ components/layout/Header/Logo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <NuxtLinkLocale v-if="logo.light && logo.dark" :to="localePath('/')" class="flex">
-      <NuxtImg :src="logo.light" class="h-7 dark:hidden" />
-      <NuxtImg :src="logo.dark" class="hidden h-7 dark:block" />
+      <NuxtImg :src="logo.light" class="h-8 dark:hidden" />
+      <NuxtImg :src="logo.dark" class="hidden h-8 dark:block" />
       <span v-if="showTitle && title" class="ml-3 self-center font-bold">
         {{ $t(title) }}
       </span>
