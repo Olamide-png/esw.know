e689f006c6f01c0e28877d4150754ce086fdd50c
Olamide-png <otoyinbo@eshopworld.com>
2025-08-22 22:17:50 +0100

    geist

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -18,7 +18,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Work Sans', 'IBM Plex Sans', 'sans-serif'],
+        sans: ['IBM Plex Sans', 'Geist', 'Work Sans', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
       },
       colors: {
