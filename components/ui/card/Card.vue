6dacb035d857845a20965d3053c1355ca9a8b623
Olamide-png <otoyinbo@eshopworld.com>
2025-08-22 09:19:00 +0100

    shadow card

--- components/ui/card/Card.vue
+++ components/ui/card/Card.vue
@@ -2,7 +2,7 @@
   <div
     :class="
       cn(
-        'rounded-lg border bg-card text-card-foreground shadow-xs',
+        'rounded-lg border bg-card text-card-foreground shadow-lg',
         props.class,
       )
     "
