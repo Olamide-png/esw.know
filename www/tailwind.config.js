28aa9f16e5c715eeddb843182efa8ab9bd4bb899
Olamide-png <otoyinbo@eshopworld.com>
2025-08-23 07:24:01 +0100

    Nunito

--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -18,7 +18,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ['Lato', 'Open Sans', 'Roboto', 'Noto Sans', 'Geist', 'Work Sans', 'sans-serif'],
+        sans: ['Nunito', 'Open Sans', 'Roboto', 'Noto Sans', 'Geist', 'Work Sans', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
       },
       colors: {
