03a15ad57816a11570246d62a0acba1a4e404192
Olamide-png <otoyinbo@eshopworld.com>
2025-08-23 09:03:42 +0100

    carbon

--- www/app.config.ts
+++ www/app.config.ts
@@ -207,7 +207,7 @@ export default defineAppConfig({
         
       ],
       carbonAds: {
-        enable: true,
+        enable: false,
         disableInDev: true,
         disableInMobile: true,
         fallback: false,
