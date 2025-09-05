97623b4ddfdd7b232fd231f33fc541a583a77c47
Olamide-png <otoyinbo@eshopworld.com>
2025-09-05 16:17:29 +0100

    tip8

--- plugins/glossary-shadcn.global.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component(
-    'GlossaryInlineShadcn',
-    defineAsyncComponent(() => import('~/components/GlossaryInlineShadcn.client.vue'))
-  )
-})
-
---
+++ plugins/glossary.global.ts
@@ -0,0 +1,6 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  // auto-register for MDC usage like ::GlossaryInline ... ::
+  nuxtApp.vueApp.component('GlossaryInline', defineAsyncComponent(() => import('~/components/GlossaryInline.client.vue')))
+})
+
+
