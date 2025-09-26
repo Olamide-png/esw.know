30c9eb814ee8357da8d81b42d27fddfa41664f22
Olamide-png <otoyinbo@eshopworld.com>
2025-09-26 09:50:11 +0000

    mcp

--- www/server/api/config-check.get.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(() => {
-  const { openaiApiKey, openaiModel } = useRuntimeConfig()
-  return {
-    // never return full secrets
-    openaiApiKeySeen: Boolean(openaiApiKey),
-    openaiModel,
-  }
-})
