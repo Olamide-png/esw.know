155ec5a02a2eec7e3572d222c0c70e8cfe14c25b
Olamide-png <otoyinbo@eshopworld.com>
2025-10-17 01:00:49 +0100

    feat: add GlossaryInline.client.vue stub to satisfy plugin import

---
+++ www/components/GlossaryInline.client.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{ term?: string; text?: string }>()
+</script>
+
+<template>
+  <span class="inline align-baseline">
+    <slot>{{ props.text || props.term || '' }}</slot>
+  </span>
+</template>
