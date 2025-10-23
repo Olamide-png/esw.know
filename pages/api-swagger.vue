a3071341c71e9929b0c55bd6210bd382e435dc46
Olamide-png <otoyinbo@eshopworld.com>
2025-10-23 15:30:34 +0100

    swagger

---
+++ pages/api-swagger.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import SwaggerUI from 'swagger-ui-dist'
+import 'swagger-ui-dist/swagger-ui.css'
+
+onMounted(() => {
+  SwaggerUI({
+    dom_id: '#swagger',
+    url: '/openapi.bundle.yaml', // from public/
+    docExpansion: 'list',
+    deepLinking: true
+  })
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <div id="swagger" class="min-h-screen dark:bg-neutral-950 dark:text-white"></div>
+  </ClientOnly>
+</template>
+
+<style>
+/* simple dark tweak */
+.dark .swagger-ui { color: #e5e7eb; }
+.dark .swagger-ui .topbar { background: transparent; }
+</style>
