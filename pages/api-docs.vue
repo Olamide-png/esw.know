34e2ed1bb1ae85836275db1d48a12be33f990204
Olamide-png <otoyinbo@eshopworld.com>
2025-10-23 13:35:56 +0100

    openapi

---
+++ pages/api-docs.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const specUrl = '/openapi.bundle.yaml' // served from /public
+</script>
+
+<template>
+  <ClientOnly>
+    <link rel="preconnect" href="https://cdn.redoc.ly" />
+    <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
+    <div class="min-h-screen">
+      <redoc :spec-url="specUrl" suppress-warnings hide-download-button />
+    </div>
+  </ClientOnly>
+</template>
+
