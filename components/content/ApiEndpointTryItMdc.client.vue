c0257f9415cbb12a9664ce738e76e84adc63b695
Olamide-png <otoyinbo@eshopworld.com>
2025-10-25 10:14:30 +0100

    tryit

---
+++ components/content/ApiEndpointTryItMdc.client.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  method: 'GET'|'POST'|'PUT'|'PATCH'|'DELETE'
+  path: string
+  baseUrl?: string
+  defaults?: any
+  allowMethodSwitch?: boolean
+}>()
+</script>
+
+<template>
+  <ApiEndpointTryIt
+    :method="method"
+    :path="path"
+    :base-url="baseUrl"
+    :defaults="defaults"
+    :allow-method-switch="allowMethodSwitch"
+  />
+</template>
